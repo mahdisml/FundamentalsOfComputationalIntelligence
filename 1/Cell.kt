20dbbe6131f132de5f391134ed95721b1b215eaa
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2019-09-27 19:47:46 +0330

    Add files via upload

---
+++ 1/Cell.kt
@@ -0,0 +1,15 @@
+class Cell(x: String) {
+
+    var result:Int
+    var teta: Double = 0.5
+
+    init {
+        val sum:Int = (x[0].toString().toInt()*-2)+(x[1].toString().toInt())+(x[2].toString().toInt()*-3)+(x[3].toString().toInt()*2)+(-x[4].toString().toInt())
+        result = if (sum > teta){
+            1
+        }else {
+            0
+        }
+    }
+
+}
