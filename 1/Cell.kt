7374bcaf570bdcb7192da75bcfb9d3a4d8ef9a12
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2019-10-01 15:37:13 +0330

    Removed Spaces

--- 1/Cell.kt
+++ 1/Cell.kt
@@ -1,7 +1,7 @@
-class Cell(x: String) {
+class Cell(x:String) {
 
     var result:Int
-    var teta: Double = 1.5
+    var teta:Double = 1.5
 
     init {
         val sum:Int = (x[0].toString().toInt()*-2)+(x[1].toString().toInt())+(x[2].toString().toInt()*-3)+(x[3].toString().toInt()*2)+(-x[4].toString().toInt())
